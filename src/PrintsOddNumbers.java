95bd9080d3896d97f36a085eea1507a7a0b4d7c0
Norayr <asoyannorayr99@gmail.com>
2023-11-04 16:58:12 +0300

    add commit

---
+++ src/PrintsOddNumbers.java
@@ -0,0 +1,22 @@
+public class PrintsOddNumbers {
+    public static void main(String[] args) {
+        int[] array = {1, 8, 7, 11, 50, 121};
+        printOddNumbers(array);
+    }
+
+    public static void printOddNumbers(int[] array) {
+        StringBuilder result = new StringBuilder();
+
+        for (int number : array) {
+            if (number % 2 != 0) {
+                result.append(number).append(", ");
+            }
+        }
+
+        if (result.length() > 0) {
+            result.delete(result.length() - 2, result.length());
+        }
+
+        System.out.println(result);
+    }
+}
